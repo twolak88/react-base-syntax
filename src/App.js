8aca64bce1ba99ea196c1dcd78dff5d14dbdd644
twolak88 <twolak88@gmail.com>
2021-02-22 20:34:32 +0100

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,64 +1,59 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [personsState, setPersons] = useState({
     persons: [
       { name: "Max", age: 28 },
       { name: "Mat", age: 23 },
-      { name: "Tom", age: 31 },
+      { name: "Tom", age: 31 }
     ]
-  };
+  });
+  const [otherState, setOtherState] = useState({
+    otherState: "other state"
+  })
+  
+  console.log(personsState, otherState)
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // DONT USE THAT: this.state.persons[0].name = "Max2";
-    this.setState({
+    setPersons({
       persons: [
         { name: "Max2", age: 28 },
         { name: "Mat2", age: 12 },
         { name: "Tom", age: 31 },
       ]
     });
-  }
+  };
 
-  render() {
-    return (
-      //parenthesis needed here to mark jsx code
-      //jsx code not html
-      //one root element
-      <div className="App">
-        <h1>Hi, this is my first react app!!</h1>
-        <h1>another heading can be add only insight App</h1>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        <Person
-          name={ this.state.persons[0].name }
-          age={ this.state.persons[0].age }
-        />
-        <Person
-          name={ this.state.persons[1].name }
-          age={ this.state.persons[1].age }
-        >
-          His hobbies: racing
-        </Person>
-        <Person
-          name={ this.state.persons[2].name }
-          age={ this.state.persons[2].age }
-        />
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'},
-    //   React.createElement('h1', null, 'Hi, this is my first react app!!')
-    // );
-  }
-}
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>This is my first react app!!</h1>
-//     </div>
-//   );
-// }
+  return (
+    //parenthesis needed here to mark jsx code
+    //jsx code not html
+    //one root element
+    <div className="App">
+      <h1>Hi, this is my first react app!!</h1>
+      <h1>another heading can be add only insight App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        His hobbies: racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'},
+  //   React.createElement('h1', null, 'Hi, this is my first react app!!')
+  // );
+};
 
 export default App;
