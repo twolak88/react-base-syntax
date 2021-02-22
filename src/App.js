64e692caac69d092ba2da8c3559fe7e739d555a1
twolak88 <twolak88@gmail.com>
2021-02-22 18:37:26 +0100

    initial project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+export default App;
