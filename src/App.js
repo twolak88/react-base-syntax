75e6472d5d6f2790b86c9ee5a2e9bb44750ec7ce
twolak88 <twolak88@gmail.com>
2021-02-22 19:33:55 +0100

    format script files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import  Person from './Person/Person'
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
-    return (//parenthesis needed here to mark jsx code
+    return (
+      //parenthesis needed here to mark jsx code
       //jsx code not html
       //one root element
       <div className="App">
         <h1>Hi, this is my first react app!!</h1>
         <h1>another heading can be add only insight App</h1>
-        <Person name="Max" age="32"/>
-        <Person name="Mat" age="23">His hobbies: racing</Person>
-        <Person name="Tom" age="33"/>
+        <Person name="Max" age="32" />
+        <Person name="Mat" age="23">
+          His hobbies: racing
+        </Person>
+        <Person name="Tom" age="33" />
       </div>
-      
     );
-    // return React.createElement('div', {className: 'App'}, 
+    // return React.createElement('div', {className: 'App'},
     //   React.createElement('h1', null, 'Hi, this is my first react app!!')
     // );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 const person = (props) => {
-    return (
-        <div>
-            <p>He is {props.name} and he is {props.age}!</p>
-            <p>{props.children}</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <p>
+        He is {props.name} and he is {props.age}!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
 
 export default person;
